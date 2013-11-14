f21d7bde132c44f60559788bb967d43bcf416c42
Date <tjjh89017@hotmail.com>
2013-11-14 15:59:05 +0800

    tpool

---
+++ include/eserv/tpool.h
@@ -0,0 +1,30 @@
+#ifndef __ESERV_TPOOL_H__
+#define __ESERV_TPOOL_H__
+
+#include <pthread.h>
+
+typedef struct _job{
+	void (*routine)(void*);
+	void *arg;
+} ex_job;
+
+typedef struct{
+	int max_threads;
+	int max_queue_size;
+	int shutdown;
+	ex_job *queue_head;
+	ex_job *queue_tail;
+	pthread_mutex_t queue_lock;
+	pthread_cond_t queue_not_empty;
+	pthread_cond_t queue_not_full;
+	pthread_cond_t queue_empty;
+
+	pthread_t *threads;
+} ex_tpool;
+
+ex_tpool* ex_tpool_new(int max_threads, int max_queue_size, int block_queue_full);
+int ex_tpool_add(ex_tpool *tpool, void (*routine)(void*), void *arg);
+int ex_tpool_free(ex_tpool *tpool, int finish_queue);
+void* ex_tpool_work(ex_tpool *tpool);
+
+#endif
